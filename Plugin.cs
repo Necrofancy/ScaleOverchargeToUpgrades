7df7cf3e757af1690c169ca939398de71f051db1
Necrofancy <31827421+Necrofancy@users.noreply.github.com>
2025-05-17 21:03:50 -0700

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace Necrofancy.Repo.ScaleOverchargeToUpgrades;
+
+[BepInPlugin(Id, Name, Version)]
+public class Plugin : BaseUnityPlugin
+{
+    const string Id = "Necrofancy.OverchargeScalesToStrength";
+    const string Name = "Overcharge Scales To Strength";
+    const string Version = "1.0.0";
+    private readonly Harmony _harmony = new(Id);
+
+    private static ConfigEntry<float>? _constantConfig;
+    private static ConfigEntry<float>? _scalingConfig;
+    
+    public static float ConstantFactor => _constantConfig?.Value ?? 0.05f;
+    public static float ScalingFactor => _scalingConfig?.Value ?? 0.15f;
+
+    public void Awake()
+    {
+        _harmony.PatchAll(Assembly.GetExecutingAssembly());
+        
+        _constantConfig = Config.Bind("Balancing", "ConstantFactor", 0.05f, "Defines starting percentage for overcharge");
+        _scalingConfig = Config.Bind("Balancing", "ScalingFactor", 0.15f, "Defines the growth per Strength upgrade applied");
+    }
+}
---
+++ ScaleOverchargeGain.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace Necrofancy.Repo.ScaleOverchargeToUpgrades;
+
+[HarmonyPatch(typeof(PhysGrabber), nameof(PhysGrabber.PhysGrabOverCharge))]
+public static class ScaleOverchargeGain
+{
+    static void Prefix(PhysGrabber __instance, ref float _amount, ref float _multiplier)
+    {
+        string id = SemiFunc.PlayerGetSteamID(__instance.playerAvatar);
+        var strengthTable = StatsManager.instance.playerUpgradeStrength;
+        if (strengthTable.TryGetValue(id, out var strength))
+            _amount *= Math.Max(Plugin.ConstantFactor + Plugin.ScalingFactor * strength, 0f);
+    }
+}
